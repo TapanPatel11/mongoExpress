dba671bb59eec226a37333fa078fda3cf7faa96c
Tapan Patel <40317792+TapanPatel11@users.noreply.github.com>
2023-07-03 17:39:56 -0300

    Update swagger.js
    updated URL

--- swagger.js
+++ swagger.js
@@ -10,7 +10,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000',
+        url: 'https://mongoservice-c3ky.onrender.com:10000',
         description: 'Development server',
       },
     ],
